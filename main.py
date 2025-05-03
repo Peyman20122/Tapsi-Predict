dcea728dea072a8f84ed7699be2c30ed64657c31
Peyman Daei Reaei <peimandaiii2012@gmail.com>
2025-05-03 09:44:05 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+import mysql.connector as msql
+from mysql.connector import Error
+
+# Load model
+model = joblib.load("model.joblib")
+
+# FastAPI init
+app = FastAPI(title="Tap30 Demand Prediction API")
+
+class DemandRequest(BaseModel):
+    day: int
+    hour: int
+    row: int
+    col: int
+
+@app.post("/predict")
+def predict_demand(data: DemandRequest):
+    features = np.array([[data.day, data.hour, data.row, data.col]])
+    prediction = int(model.predict(features)[0])
+    try:
+        # Connect to the database
+        conn = msql.connect(
+            host='127.0.0.1',
+            user='root',
+            password='peiman2012',
+            database='tap30db'
+        )
+        if conn.is_connected():
+            cursor = conn.cursor()
+
+            # Create table if not exists
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS demand_predictions (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    Day INT,
+                    Hour INT,
+                    row_val INT,
+                    col_val INT,
+                    predicted_demand INT
+                )
+            """)
+
+            # Insert prediction record
+            insert_query = """
+                INSERT INTO demand_predictions (Day, Hour,row_val, col_val, predicted_demand)
+                VALUES (%s, %s, %s, %s, %s)
+            """
+            cursor.execute(insert_query, (data.day, data.hour, data.row, data.col, prediction))
+            conn.commit()
+
+    except Error as e:
+        return {"error": str(e)}
+
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+
+    return {
+        "input_features": data.dict(),
+        "predicted_demand": prediction
+    }
+
